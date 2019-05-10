871f5b7e030f67d9ba16a346546503629f6a5e43
Angela Mumba <malaika.mumba@gmail.com>
2019-05-10 20:39:39 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,13 +1,14 @@
 require 'pry'
-birthday_kids = {
-  "Timmy" => 9, 
-  "Sarah" => 6, 
-  "Amanda" => 27
-}
+# birthday_kids = {
+#   "Timmy" => 9, 
+#   "Sarah" => 6, 
+#   "Amanda" => 27
+# }
 
 def happy_birthday(birthday_kids)
+  binding.pry
   birthday_kids.each do |kids_name, age|
-    binding.pry
+    # binding.pry
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   
   end
