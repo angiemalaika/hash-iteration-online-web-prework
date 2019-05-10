adba111f5cd97d8c25b5101599752bceda47e028
Angela Mumba <malaika.mumba@gmail.com>
2019-05-10 20:28:34 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,5 +1,4 @@
 require 'pry'
-
 birthday_kids = {
   "Timmy" => 9, 
   "Sarah" => 6, 
@@ -19,30 +18,9 @@ def age_appropriate_birthday(birthday_kids)
     else
       puts "You are too old for this."
       
-      require 'pry'
-birthday_kids = {
-  "Timmy" => 9, 
-  "Sarah" => 6, 
-  "Amanda" => 27
-}
-
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
-
-def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    if age <= 12
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else
-      puts "You are too old for this."
     end
-      end
- binding.pry
-end
-    end
+    
+    binding.pry
       end
 
 end
